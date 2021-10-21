f5b98c324e04c104055b149f4abdbcade58c6db5
shubham7811 <shubham7811@yahoo.com>
2021-10-21 20:59:22 +0530

    File Structure changes and added more algos

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -3,7 +3,7 @@ import './footer.css'
 export function Footer() {
 	return (
 		<div className='footer col-12'>
-			Copyright &#169; 2021 Shubham Jindal
+			Copyright &#169; 2021 <a href="https://jindal2209.github.io/mysite">Shubham Jindal</a>
 		</div>
 	)
 }
